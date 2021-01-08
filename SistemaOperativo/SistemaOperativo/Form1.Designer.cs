2464feb4d7341f1114fdaccf949c0333a620a93b
Geysler Andrew Nic Yah <67171361+Andrew-Nic@users.noreply.github.com>
2021-01-08 17:36:55 -0600

    Add files via upload

--- SistemaOperativo/SistemaOperativo/Form1.Designer.cs
+++ SistemaOperativo/SistemaOperativo/Form1.Designer.cs
@@ -29,7 +29,61 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inicio = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SistemaOperativo.Properties.Resources._91zW_5pkflL__AC_SY355_;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 776);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inicio
+            // 
+            this.inicio.Location = new System.Drawing.Point(16, 732);
+            this.inicio.Name = "inicio";
+            this.inicio.Size = new System.Drawing.Size(53, 42);
+            this.inicio.TabIndex = 1;
+            this.inicio.Text = "INICIO\r\n";
+            this.inicio.UseVisualStyleBackColor = true;
+            this.inicio.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 656);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "BLOG DE NOTAS";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 594);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "IMAGENES";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 531);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ARCHIVOS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -37,17 +91,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1368, 768);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.inicio);
+            this.Controls.Add(this.pictureBox1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button inicio;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- SistemaOperativo/SistemaOperativo/Form1.cs
+++ SistemaOperativo/SistemaOperativo/Form1.cs
@@ -16,10 +16,37 @@ namespace SistemaOperativo
         {
             InitializeComponent();
         }
-
+        private Form2 frm2;
         private void Form1_Load(object sender, EventArgs e)
+        {
+            this.inicio.Enabled = true;
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
+        int cont = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cont < 1 ){
+            if (frm2 == null || frm2.IsDisposed)
+            {
+                frm2 = new Form2();
+            }
+            frm2.Show();
+                cont++;
+            }
+            else
+            {
+                frm2.Close();
+                cont = 0;
+            }
+           
+
+        }
+       
+
     }
 }
